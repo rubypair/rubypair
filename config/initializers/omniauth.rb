2cdd2725b76c26f3632b181f98e32ef12d4ce7c7
Evan Light <evan@tripledogdare.net>
2011-08-20 02:40:05 -0400

    Forgot to fix the path relative to the initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   begin
-    APP_CONFIG = YAML.load(File.read(File.expand_path('../app_config.yml', __FILE__)))
+    APP_CONFIG = YAML.load(File.read(File.expand_path('../../app_config.yml', __FILE__)))
     provider :github, APP_CONFIG['omniauth']['github_id'], APP_CONFIG['omniauth']['github_secret']
   rescue
     provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET']
