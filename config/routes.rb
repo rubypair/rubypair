759afd6d30c641e8b3ca88b8771f5fa138dc265f
Evan Light <evan@tripledogdare.net>
2011-08-20 11:18:01 -0400

    Adds search field
    Logic TBD

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rubypair::Application.routes.draw do
   match '/auth/:provider/callback' => 'sessions#create'
   match '/signout' => 'sessions#destroy'
 
+  match '/search' => 'home#search'
+
   root :to => "home#index"
 
   resources :profiles, :only => [:edit, :update, :show]
