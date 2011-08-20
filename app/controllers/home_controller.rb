908b6231ed5fcadf7c31b91138b8101401a93052
Evan Light <evan@tripledogdare.net>
2011-08-19 20:32:39 -0400

    Newest local and neweset remote on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @newest_users = User.all(sort: :created_at).limit(5)
+    @newest_remote_users = User.desc(:created_at).where("profile.interested_in_remote" => true).limit(5)
+    @newest_local_users = User.desc(:created_at).where("profile.interested_in_local" => true).limit(5)
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,7 @@ class Profile
   include Mongoid::Document
 
   field :interested_in_remote, type: Boolean, default: false
+  field :interested_in_local, type: Boolean, default: false
 
   embedded_in :user
 end
