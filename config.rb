56e2c3d774153e3a2a34513c1ba12007b04d4e77
Evan Light <evan@tripledogdare.net>
2011-08-24 22:42:33 -0400

    Reconfigured SASS to play nicely with asset pipeline (w/ tundal45)

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'ninesixty'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/stylesheets"
+css_dir = "/app/assets/stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
