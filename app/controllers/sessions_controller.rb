abc13964968f7888122509baf2539ab2bf62fe15
Sam Elliott <sam@lenary.co.uk>
2011-09-11 22:11:51 +0100

    Geocode users on initial sign-up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,11 @@
+require 'geocoder'
+
 class SessionsController < ApplicationController
   def create
     user_info = request.env['omniauth.auth']['user_info']
     user_hash = request.env['omniauth.auth']['extra']['user_hash']
+
+    geocoder = Geocoder.new
 
     login = user_hash['login']
     user = User.where(github_login: login).first
@@ -12,6 +16,8 @@ class SessionsController < ApplicationController
       session[:github_login] = user.github_login
       redirect_target = :root
     else
+      geocoder = Geocoder.new
+
       user = User.new
       user.github_login = login
       user.name = user_info['name']
@@ -20,6 +26,8 @@ class SessionsController < ApplicationController
       user.blog_url = user_info['urls']['Blog']
       user.gravatar_id = user_hash['gravatar_id']
       user.location = user_hash['location']
+      # do we want to introduce this delay?
+      user.latlong = geocoder.geocode!(user_hash['location'])
       user.save!
       session[:github_login] = user.github_login
       redirect_target = edit_user_path(user.id)
