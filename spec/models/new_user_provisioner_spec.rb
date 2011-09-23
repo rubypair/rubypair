00ade397c66a7f0e4ebb8163aec0256b5e6d4e77
Evan Light <evan@tripledogdare.net>
2011-09-22 23:51:54 -0400

    Paired with jszmajda on refactoring session controller using DCI

---
+++ spec/models/new_user_provisioner_spec.rb
@@ -0,0 +1,34 @@
+$LOAD_PATH << "."
+
+require 'ostruct'
+require 'app/models/new_user_provisioner'
+
+describe NewUserProvisioner do
+  subject do
+    OpenStruct.new.tap do |user|
+      user.extend NewUserProvisioner
+      @fake_user_info = {
+        'name' => 'jsmith',
+        'email' => 'jsmith@gmail.com',
+        'urls' => {
+          'GitHub' => 'http://www.github.com/jsmith',
+          'Blog' => 'http://awesomeblog.com',
+        }
+      }
+      @fake_extra_user_hash = {
+        'login' => 'jsmith',
+        'gravatar_id' => 'other_email@gmail.com',
+        'location' => 'someplace'
+      }
+      user.provision_with(@fake_user_info, @fake_extra_user_hash)
+    end
+  end
+
+  its(:name) { should == 'jsmith' }
+  its(:email) { should == 'jsmith@gmail.com' }
+  its(:github_url) { should == 'http://www.github.com/jsmith' }
+  its(:github_login) { should == 'jsmith' }
+  its(:blog_url) { should == 'http://awesomeblog.com' }
+  its(:gravatar_id) { should == 'other_email@gmail.com' }
+  its(:location) { should == 'someplace' }
+end
