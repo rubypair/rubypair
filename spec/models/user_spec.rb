a2584972f0d89c398e55fa418e0af8f970dba2c4
Diego Scataglini <diego@junivi.com>
2011-10-05 00:46:57 -0400

    removing if statement for more functional style

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,14 +4,19 @@ describe User do
   let (:user) { Factory.build(:user) }
 
   describe "twitter handle" do
-    it "strips a prefixed @-symbol" do
-      user.twitter = "@Lenary"
+    it "copes with a prefixed @-symbol & space padded handle" do
+      user.twitter = " @Lenary "
       user.twitter.should == "Lenary"
-    end
+    end    
 
     it "copes with no prefixed @-symbol" do
       user.twitter = "Lenary"
       user.twitter.should == "Lenary"
+    end
+    
+    it "copes with empty strings" do
+      user.twitter = ""
+      user.twitter.should == ""
     end
   end
 end
