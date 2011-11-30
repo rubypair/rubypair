ad573000603097748f0e18f2876ac2f924c22a07
Evan Light <evan@tripledogdare.net>
2011-11-29 23:43:08 -0500

    Added TODOs for this branch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
   def availability_links
+    # TODO: Consider embedding haml engine call here so that we can use HAML instead of clusterfuck that is content_tag
+    # TODO: Then extract the haml engine call out into a method that takes a string of HAML but handles all of the details of setting up the engine
     content_tag(:span) do
       link_to("Mark Available", user_availability_path(current_user), method: :post, class: "available #{availability_css_class(:available)}", remote: true) +
       link_to("Mark Unvailable", user_availability_path(current_user), method: :delete, class: "unavailable #{availability_css_class(:unavailable)}", remote: true)
