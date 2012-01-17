8f02e23883938cb63f4b56c9a1d629b1cb3016a1
Gordon DuQuesnay & Sam Elliott <sam@lenary.co.uk>
2012-01-17 10:47:58 +0000

    Refactor and fix user availability

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,41 +25,4 @@ describe User do
     end
   end
 
-  context "availability" do
-    describe "When searching for available user" do
-      before do
-        @not_available = Factory(:user, last_available_time: nil)
-        @available = Factory(:user, last_available_time: Time.now)
-      end
-
-      it "should only find users with a available mark up time" do
-        result = User.available
-        result.should include(@available)
-        result.should_not include(@not_available)
-      end
-
-      context "who have marked available recently" do
-        before do
-          @available_by_1_sec =
-            Factory(:user,
-                    last_available_time: Time.now - User::RECENT_AVAILABILILTY_OFFSET + 1.second)
-          @available_exactly =
-            Factory(:user,
-                    last_available_time: Time.now - User::RECENT_AVAILABILILTY_OFFSET)
-          @not_available_by_1_sec =
-            Factory(:user,
-                    last_available_time: Time.now - User::RECENT_AVAILABILILTY_OFFSET - 1.second)
-        end
-
-        it "should only find users with a markup time within the recent constraint" do
-          result = User.available_recently
-          result.should include(@available_by_1_sec)
-          result.should include(@available_exactly)
-          result.should include(@available)
-          result.should_not include(@not_available_by_1_sec)
-        end
-      end
-    end
-  end
-
 end
