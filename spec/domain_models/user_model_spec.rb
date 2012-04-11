6e4e27d64b9377231a3ced54de94938a01034990
Evan Light <evan@tripledogdare.net>
2012-04-10 23:43:14 -0400

    Merged fixed_right_now into master pairing with samnang

--- spec/domain_models/user_model_spec.rb
+++ spec/domain_models/user_model_spec.rb
@@ -52,4 +52,47 @@ describe UserModel do
       UserModel.tag_cloud(3).should == expected
     end
   end
+
+  describe "When searching for available users" do
+    it "should only find users with a available mark up time" do
+      Timecop.freeze(Time.now) do
+        available = Factory(:user, last_available_time: Time.now)
+        never_available = Factory(:user, last_available_time: nil)
+
+        result = UserModel.ever_been_available
+        result.should include(available)
+        result.should_not include(never_available)
+      end
+    end
+
+    context "who have marked available recently" do
+      before do
+        Timecop.freeze(Time.now)
+        @available_by_1_sec =
+          Factory(:user, last_available_time: UserModel.currently_available_offset + 1.second)
+        @available_exactly =
+          Factory(:user, last_available_time: UserModel.currently_available_offset)
+        @not_available_by_1_sec =
+          Factory(:user, last_available_time: UserModel.currently_available_offset - 1.second)
+      end
+
+      after do
+        Timecop.return
+      end
+
+      it "should only find users with a markup time within the recent constraint" do
+        result = UserModel.currently_available
+        result.should include(@available_by_1_sec)
+        result.should include(@available_exactly)
+        result.should_not include(@not_available_by_1_sec)
+      end
+
+      it "should sort the available users by last_available_time descending" do
+        result = UserModel.currently_available
+        result.length.should == 2
+        result[0].should == @available_by_1_sec
+        result[1].should == @available_exactly
+      end
+    end
+  end
 end
